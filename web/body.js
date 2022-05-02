284a4a94772635b9ba6f7bbd3a6b4c5733b60987
unknown <6222780270@g.siit.tu.ac.th>
2022-05-02 13:59:30 +0700

    Added h1 tag

--- web/body.js
+++ web/body.js
@@ -9,27 +9,29 @@ fetch(serverURL+"/content/"+contentType, {
 	response.json().then(function(json){
 		var data = json;
 		var div = document.createElement("div");
-		div.setAttribute("class","container-fluid bg-grey");
+		div.setAttribute("class","container-fluid");
 		content.innerHTML = "";
 		for(var i = 0; i < data.length; i++){
 			var div2 = document.createElement("div");
 			div2.setAttribute("id", data[i]['id']);
-			var contentTitle = document.createElement("h2");
+			var contentTitle = document.createElement("h1");
 			contentTitle.innerHTML = data[i]['title'];
-			var contentBody = document.createElement("div");
+			var contentBody = document.createElement("h3");
 			contentBody.innerHTML = data[i]['content'];
+			var lineBreak = document.createElement("br");
 			div2.appendChild(contentTitle);
 			div2.appendChild(contentBody);
-for (var j =0;j<data[i]['file'].length;j++){
+			div2.appendChild(lineBreak);
+			for (var j =0;j<data[i]['file'].length;j++){
 				var contentFile = document.createElement("img");
 				contentFile.setAttribute("src", data[i]['file'][j]['path']);
-contentFile.setAttribute("alt", data[i]['file'][j]['description']);
-				contentFile.setAttribute("width", 400);
-				contentFile.setAttribute("height", 400);
+				contentFile.setAttribute("alt", data[i]['file'][j]['description']);
+				contentFile.setAttribute("width", 300);
 				div2.appendChild(contentFile);
 			}
+			div2.style.paddingBottom = "80px";
 			div.appendChild(div2);
-contentList.push(div2);
+			contentList.push(div2);
 		}
 		content.appendChild(div);
 	})
@@ -44,7 +46,7 @@ fetch(serverURL+"/user/me", {
 .then(function(response){
 	if(response.status == 403){
 			head.innerHTML = `
-				<img src="./files/test_logo.png" alt="Test Logo" size="250px" width="250px">
+				<img src="./files/test_logo.png" alt="Test Logo" size="200px" width="200px">
 				<h1>Welcome</h1>
 			`;
 return;
@@ -52,25 +54,41 @@ return;
 response.json().then(function(json){
 		for(var i=0;i<contentList.length;i++){
 			var div = document.createElement("div");
-var edit = document.createElement("button");
-edit.setAttribute("onClick", "redirect('edit.html?contentType="+contentType+"&id="+contentList[i].getAttribute("id")+"')");
-edit.innerHTML = "Edit";
-div.appendChild(edit);
-contentList[i].appendChild(div);
+			var edit = document.createElement("button");
+			edit.setAttribute("onClick", "redirect('edit.html?contentType="+contentType+"&id="+contentList[i].getAttribute("id")+"')");
+			edit.style.fontSize = "20px";
+			edit.style.paddingLeft = "10px";
+			edit.style.paddingRight = "10px";
+			edit.innerHTML = "Edit";
+			div.style.paddingTop = "30px";
+			div.appendChild(edit);
+			div.style.paddingBottom = "10px";
+			contentList[i].appendChild(div);
 
 			var div = document.createElement("div");
 			var del = document.createElement("button");
-del.setAttribute("onClick", "handleContentDelete("+contentList[i].getAttribute("id")+")");
+			del.setAttribute("onClick", "handleContentDelete("+contentList[i].getAttribute("id")+")");
+			del.style.fontSize = "20px";
+			del.style.paddingLeft = "10px";
+			del.style.paddingRight = "10px";
 			del.innerHTML = "Delete";
 			div.appendChild(del);
-contentList[i].appendChild(div);
+			contentList[i].appendChild(div);
 		}
 var name = json.username;
 			head.innerHTML = `
-				<img src="./files/test_logo.png" alt="Test Logo" size="250px" width="250px">
-				<h1>Welcome `+name+`</h1>
-				<button style="color:black;" onClick="handleLogout()">Logout</button>
-				<button style="color:black;" onClick="redirect('add.html?contentType=`+contentType+`')">Add Content</button>
+			<style>
+				.button	{
+					color: black;
+					font-size: 20px;
+					padding-left: 10px;
+					padding-right: 10px;
+				}
+			</style>
+			<img src="./files/test_logo.png" alt="Test Logo" size="200px" width="200px">
+			<h1>Welcome `+name+`</h1><br>
+			<button class="button" onClick="handleLogout()">Logout</button>&nbsp;
+			<button class="button" onClick="redirect('add.html?contentType=`+contentType+`')">Add Content</button>
 			`;
 })
 })
